d40c3c06379fa34e86d76d4d790ac54a2f235dd8
jjbb1221 <41380360+jjbb1221@users.noreply.github.com>
2018-07-28 17:58:08 -0500

    Update bot.js

---
+++ bot.js
@@ -0,0 +1,40 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const fs = require('fs');
+const config = require('./settings.json');
+require('./util/eventLoader')(client);
+
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+fs.readdir('./commands/', (err, files) => {
+  if (err) console.error(err);
+  files.forEach(f => {
+    let props = require(`./commands/${f}`);
+    client.commands.set(props.help.name, props);
+    props.conf.aliases.forEach(alias => {
+      client.aliases.set(alias, props.help.name);
+    });
+  });
+});
+
+client.reload = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      delete require.cache[require.resolve(`./commands/${command}`)];
+      let cmd = require(`./commands/${command}`);
+      client.commands.delete(command);
+      client.aliases.forEach((cmd, alias) => {
+        if (cmd === command) client.aliases.delete(alias);
+      });
+      client.commands.set(command, cmd);
+      cmd.conf.aliases.forEach(alias => {
+        client.aliases.set(alias, cmd.help.name);
+      });
+      resolve();
+    } catch (e){
+      reject(e);
+    }
+  });
+};
+
+client.login(process.env.TOKEN)
