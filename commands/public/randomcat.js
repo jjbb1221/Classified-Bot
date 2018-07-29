7217ecf14f174f5198c7de22d4850644bb096f00
jjbb1221 <41380360+jjbb1221@users.noreply.github.com>
2018-07-29 10:35:28 -0500

    Delete randomcat.js

--- commands/public/randomcat.js
+++
@@ -1,6 +0,0 @@
-Discord.on('message', function (message) {
-		if (message.content.startsWith('/randomcat') && true){
- const randomcat = 'https://i.imgur.com/jjqKt7t.gifv';
-   return message.reply(`Found you this random cat: ${randomcat}`) }
-	return randomcat
- return 'hi'
