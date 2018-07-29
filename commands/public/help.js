6d29246657fa7f8899767dd981d400f2eb8a9996
jjbb1221 <41380360+jjbb1221@users.noreply.github.com>
2018-07-29 10:35:03 -0500

    Delete help.js

--- commands/public/help.js
+++
@@ -1,4 +0,0 @@
-Discord.on('message', function (message) {
-			if (message.content === '/help') {
-				return message.author.send('Commands: `/help`, `/play`, `/pause`, `/stop`, `/about`, `/thonk`, `/cat`, `/ping`, `/8ball`, `/roll`, `/eval`, `/say`, `/randomcat`');
-			  }
