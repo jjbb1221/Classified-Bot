c1093d7b15e5326c59188088ab98f25c7f71a2a5
jjbb1221 <41380360+jjbb1221@users.noreply.github.com>
2018-07-29 10:34:38 -0500

    Delete cat.js

--- commands/public/cat.js
+++
@@ -1,4 +0,0 @@
-Discord.on('message',  function (message) {
-  const cat = request.get('https://aws.random.cat/meow');
-   if(message.content === '/cat') {
-	cat.then(r => message.reply(r.body.file));
